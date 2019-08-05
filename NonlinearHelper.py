5d327c89b993535a01392e481abe999d16544a74
Federico <fp267@cornell.edu>
2019-08-05 15:09:18 -0400

    save commonly used functions for squeezing calculations, covariance, transformations etc

--- NonlinearHelper.py
+++ NonlinearHelper.py
@@ -1,4 +1,7 @@
 import numpy as np
+from numpy.fft import fft, ifft, fftshift, ifftshift
+from scipy.linalg import det, sqrtm, inv, eig
+
 
 def calculateLengthScales(gamma, peakPower, beta2, timeScale, pulseTypeFWHM=None, refractiveInd=1):
   """
@@ -19,6 +22,46 @@ def calculateLengthScales(gamma, peakPower, beta2, timeScale, pulseTypeFWHM=None
     DS /= 4 * np.log(2)
     # DS /= 8 * np.log(2)
   return NL * refractiveInd, DS * refractiveInd
+
+
+def calcQuadratureGreens(greenC, greenS):
+  Z = np.block([[np.real(greenC + greenS), -np.imag(greenC - greenS)],
+                [np.imag(greenC + greenS),  np.real(greenC - greenS)]]).astype(dtype=np.float_)
+
+  return Z
+
+
+def calcCovarianceMtx(Z, tol=1e-4):
+  cov = Z @ Z.T
+  determinant = det(cov)
+  assert abs(determinant - 1) < tol, "det(C) = %f" % determinant
+  return cov
+
+
+def calcLOSqueezing(C, pumpTimeProf):
+  freqDomain = fftshift(fft(ifftshift(pumpTimeProf)))
+
+  localOscillX = np.hstack([freqDomain.real,  freqDomain.imag]) / np.linalg.norm(freqDomain)
+  localOscillP = np.hstack([freqDomain.imag, -freqDomain.real]) / np.linalg.norm(freqDomain)
+
+  covMatrix = np.zeros((2, 2))
+  covMatrix[0,0] = localOscillX.T @ C @ localOscillX
+  covMatrix[1,1] = localOscillP.T @ C @ localOscillP
+  covMatrix[0,1] = covMatrix[1,0] = (localOscillX.T @ C @ localOscillP + localOscillP.T @ C @ localOscillX) / 2
+
+  variances = np.linalg.eigvals(covMatrix)
+
+  variances[0], variances[1] = np.min(variances), np.max(variances)
+  return variances
+
+
+# simpler version than Xanadu
+def blochMessiahEigs(Z):
+  sigma = sqrtm(Z @ Z.T)
+  eigenvalues, eigenvectors = eig(sigma)
+
+  sortedEig = np.sort(eigenvalues).real
+  return sortedEig
 
 
 if __name__ == "__main__":
