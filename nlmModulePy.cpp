8a180c1bb7dca59547f3e38c35edd0f72ebcc76f
Federico <fp267@cornell.edu>
2021-02-03 12:48:21 -0500

    Add docstrings to the nonlinearmedium module

--- nlmModulePy.cpp
+++ nlmModulePy.cpp
@@ -11,23 +11,24 @@ PYBIND11_MODULE(nonlinearmedium, m) {
   namespace py = pybind11;
   using namespace pybind11::literals;
 
-  py::class_<_NonlinearMedium> _NLMBase(m, "_NonlinearMedium");
-  py::class_<Chi3, _NonlinearMedium> Chi3(m, "Chi3");
-  py::class_<_Chi2, _NonlinearMedium> _Chi2Base(m, "_Chi2");
-  py::class_<Chi2PDC, _Chi2> Chi2PDC(m, "Chi2PDC");
-  py::class_<Chi2SHG, _Chi2> Chi2SHG(m, "Chi2SHG");
-  py::class_<Chi2SFGPDC, _Chi2> Chi2SFGPDC(m, "Chi2SFGPDC");
-  py::class_<Chi2SFG, _Chi2> Chi2SFG(m, "Chi2SFG");
-  py::class_<Chi2PDCII, _Chi2> Chi2PDCII(m, "Chi2PDCII");
-  py::class_<Chi2SFGII, _Chi2> Chi2SFGII(m, "Chi2SFGII");
+  m.doc() = "Module for numerical simulation of classical or quantum signals in pumped nonlinear media.";
+
+  py::class_<_NonlinearMedium> _NLMBase(m, "_NonlinearMedium", "Base class for nonlinear medium solvers");
+  py::class_<Chi3, _NonlinearMedium> Chi3(m, "Chi3", "Single mode self phase modulation");
+  py::class_<_Chi2, _NonlinearMedium> _Chi2Base(m, "_Chi2", "Base class for Chi(2) solvers");
+  py::class_<Chi2PDC, _Chi2> Chi2PDC(m, "Chi2PDC", "Degenerate optical parametric amplification");
+  py::class_<Chi2SHG, _Chi2> Chi2SHG(m, "Chi2SHG", "Second harmonic generation");
+  py::class_<Chi2SFGPDC, _Chi2> Chi2SFGPDC(m, "Chi2SFGPDC", "Simultaneous sum frequency generation and parametric amplification");
+  py::class_<Chi2SFG, _Chi2> Chi2SFG(m, "Chi2SFG", "Sum (or difference) frequency generation");
+  py::class_<Chi2PDCII, _Chi2> Chi2PDCII(m, "Chi2PDCII", "Type II or nondegenerate optical parametric amplification");
+  py::class_<Chi2SFGII, _Chi2> Chi2SFGII(m, "Chi2SFGII", "Type II or simultaneous 2-mode sum frequency generation with parametric amplification");
   py::class_<Cascade, _NonlinearMedium> Cascade(m, "Cascade");
 
 
-  // default arguments for Python initialization of empty arrays
+  // default arguments for Python, including initialization of empty arrays
   Eigen::Ref<const Arraycd> defArraycd = Eigen::Ref<const Arraycd>(Arraycd{});
   Eigen::Ref<const Arrayd>  defArrayf  = Eigen::Ref<const Arrayd>(Arrayd{});
   const std::vector<char> defCharVec = {};
-
   constexpr double infinity = std::numeric_limits<double>::infinity();
 
 /*
@@ -36,35 +37,75 @@ PYBIND11_MODULE(nonlinearmedium, m) {
 
   _NLMBase.def("setPump",
                py::overload_cast<int, double, double>(&_NonlinearMedium::setPump),
+               "Set the input shape of the pump\n"
+               "pulseType Gaussian, Sech or Sinc profile; 0, 1, 2 respectively.\n"
+               "chirp     Initial chirp of the pump, specified in dispersion lengths.\n"
+               "delay     Initial time delay of the pump, specified in walk-off lengths.\n",
                "pulseType"_a, "chirp"_a = 0, "delay"_a = 0);
 
   _NLMBase.def("setPump",
                py::overload_cast<const Eigen::Ref<const Arraycd>&, double, double>(&_NonlinearMedium::setPump),
+               "Set the input shape of the pump\n"
+               "customPump An arbitrary pump shape specified in the time domain, with self.tau as the axis.\n"
+               "chirp     Initial chirp of the pump, specified in dispersion lengths.\n"
+               "delay     Initial time delay of the pump, specified in walk-off lengths.\n",
                "customPump"_a, "chirp"_a = 0, "delay"_a = 0);
 
-  _NLMBase.def("runPumpSimulation", &_NonlinearMedium::runPumpSimulation);
+  _NLMBase.def("runPumpSimulation", &_NonlinearMedium::runPumpSimulation,
+               "Simulate propagation of the pump field");
 
   _NLMBase.def("runSignalSimulation",
                py::overload_cast<const Eigen::Ref<const Arraycd>&, bool, uint>(&_NonlinearMedium::runSignalSimulation),
+               "Simulate propagation of the signal field(s)\n"
+               "inputProf    Profile of input pulse. May be time or frequency domain.\n"
+               "             Note: Input is assumed to have self.omega or self.tau as the axis.\n"
+               "inTimeDomain Specify if the input is in time or frequency domain.\n"
+               "inputMode    Specify which signal mode of the nonlinear medium the input corresponds to.\n",
                "inputProf"_a, "inTimeDomain"_a = true, "inputMode"_a = 0);
 
   _NLMBase.def("computeGreensFunction",
                &_NonlinearMedium::computeGreensFunction, py::return_value_policy::move,
+               "Solve the Green's function matrices C and S where a(L) = C a(0) + S [a(0)]^t\n"
+               "inTimeDomain Compute the Green's function in time or frequency domain.\n"
+               "runPump      Whether to run the pump simulation before signal simulations.\n"
+               "nThreads     Number of threads used to run simulations in parallel.\n"
+               "normalize    Whether to adjust the final matrix so that amplitudes are converted 1-to-1 between different modes.\n"
+               "useInput     Specify which inputs modes to include in the transformation. Default is all inputs.\n"
+               "useOutput    Specify which output modes to include in the transformation. Default is all outputs.\n"
+               "return: Green's function matrices C, S\n",
                "inTimeDomain"_a = false, "runPump"_a = true, "nThreads"_a = 1, "normalize"_a = false,
                "useInput"_a = defCharVec, "useOutput"_a = defCharVec);
 
   _NLMBase.def("batchSignalSimulation",
-               &_NonlinearMedium::batchSignalSimulation, py::return_value_policy::move, "inputProfs"_a,
-               "inTimeDomain"_a = false, "runPump"_a = true, "nThreads"_a = 1, "inputMode"_a = 0, "useOutput"_a = defCharVec);
+               &_NonlinearMedium::batchSignalSimulation, py::return_value_policy::move,
+               "Run multiple signal simulations.\n"
+               "inputProfs   Profiles of input pulses. May be time or frequency domain.\n"
+               "             Note: Input is assumed to have self.omega or self.tau as the axis.\n"
+               "inTimeDomain Specify if the input is in time or frequency domain.\n"
+               "runPump      Whether to run the pump simulation before signal simulations.\n"
+               "nThreads     Number of threads used to run simulations in parallel.\n"
+               "inputMode    Specify which signal mode of the nonlinear medium the input corresponds to.\n"
+               "useOutput    Specify which signal mode outputs to return. Default is all outputs.\n"
+               "return: Array of signal profiles at the output of the medium\n",
+               "inputProfs"_a, "inTimeDomain"_a = false, "runPump"_a = true, "nThreads"_a = 1,
+               "inputMode"_a = 0, "useOutput"_a = defCharVec);
 
-  _NLMBase.def_property_readonly("pumpFreq", &_NonlinearMedium::getPumpFreq, py::return_value_policy::reference);
-  _NLMBase.def_property_readonly("pumpTime", &_NonlinearMedium::getPumpTime, py::return_value_policy::reference);
-  _NLMBase.def_property_readonly("omega", &_NonlinearMedium::getFrequency, py::return_value_policy::reference);
-  _NLMBase.def_property_readonly("tau", &_NonlinearMedium::getTime, py::return_value_policy::reference);
-  _NLMBase.def_property_readonly("signalFreq", [](_NonlinearMedium& nlm){return nlm.getSignalFreq();}, py::return_value_policy::reference);
-  _NLMBase.def_property_readonly("signalTime", [](_NonlinearMedium& nlm){return nlm.getSignalTime();}, py::return_value_policy::reference);
-  _NLMBase.def("signalFreqs", &_NonlinearMedium::getSignalFreq, py::return_value_policy::reference, "i"_a = 0);
-  _NLMBase.def("signalTimes", &_NonlinearMedium::getSignalTime, py::return_value_policy::reference, "i"_a = 0);
+  _NLMBase.def_property_readonly("pumpFreq", &_NonlinearMedium::getPumpFreq, py::return_value_policy::reference,
+                                 "Read-only array of the pump frequency profile along the length of the medium.");
+  _NLMBase.def_property_readonly("pumpTime", &_NonlinearMedium::getPumpTime, py::return_value_policy::reference,
+                                 "Read-only array of the pump time profile along the length of the medium.");
+  _NLMBase.def_property_readonly("omega", &_NonlinearMedium::getFrequency, py::return_value_policy::reference,
+                                 "Read-only frequency axis of the system.");
+  _NLMBase.def_property_readonly("tau", &_NonlinearMedium::getTime, py::return_value_policy::reference,
+                                 "Read-only time axis of the system");
+  _NLMBase.def_property_readonly("signalFreq", [](_NonlinearMedium& nlm){return nlm.getSignalFreq();}, py::return_value_policy::reference,
+                                 "Read-only array of the signal frequency profile along the length of the medium.");
+  _NLMBase.def_property_readonly("signalTime", [](_NonlinearMedium& nlm){return nlm.getSignalTime();}, py::return_value_policy::reference,
+                                 "Read-only array of a signal time profile along the length of the medium.");
+  _NLMBase.def("signalFreqs", &_NonlinearMedium::getSignalFreq, py::return_value_policy::reference,
+               "Read-only array of a signal frequency profile along the length of the medium.", "i"_a = 0);
+  _NLMBase.def("signalTimes", &_NonlinearMedium::getSignalTime, py::return_value_policy::reference,
+               "Read-only array of a signal time profile along the length of the medium.", "i"_a = 0);
 
 
 /*
@@ -76,14 +117,16 @@ PYBIND11_MODULE(nonlinearmedium, m) {
       "relativeLength"_a, "nlLength"_a, "beta2"_a, "customPump"_a = defArraycd, "pulseType"_a = 0,
       "beta3"_a = 0, "rayleighLength"_a = infinity, "tMax"_a = 10, "tPrecision"_a = 512, "zPrecision"_a = 100, "chirp"_a = 0);
 
-  Chi3.def("runPumpSimulation", &Chi3::runPumpSimulation);
+  Chi3.def("runPumpSimulation", &Chi3::runPumpSimulation,
+           "Simulate propagation the of pump field");
 
 
 /*
  * _Chi2
  */
 
-  _Chi2Base.def_property_readonly("poling", &_Chi2::getPoling, py::return_value_policy::reference);
+  _Chi2Base.def_property_readonly("poling", &_Chi2::getPoling, py::return_value_policy::reference,
+                                  "Read-only array of the domain poling along the length of the Chi(2) medium.");
 
 
 /*
