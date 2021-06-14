d31ac5d5b9be469891693fb20671aa1ff18adf9a
Federico <fp267@cornell.edu>
2021-06-14 12:59:49 -0400

    checks for slightly safer combinePoling procedure

--- NonlinearHelper.py
+++ NonlinearHelper.py
@@ -400,20 +400,20 @@ def combinePoling(polingA, polingB, tol):
           cumulative += polingA[indexA]
           indexA += 1
           remainingA = polingA[indexA % polingA.size]
-      combinedDomains.append(cumulative)
+      if cumulative > 0: combinedDomains.append(cumulative)
 
     if remainingA > remainingB:
-      combinedDomains.append(remainingB)
+      if remainingB > 0: combinedDomains.append(remainingB)
       remainingA -= remainingB
       indexB += 1
       remainingB = polingB[indexB % polingB.size]
     else:
-      combinedDomains.append(remainingA)
+      if remainingA > 0: combinedDomains.append(remainingA)
       remainingB -= remainingA
       indexA += 1
       remainingA = polingA[indexA % polingA.size]
 
-  combinedDomains.append(min(remainingA, remainingB))
+  if min(remainingA, remainingB) > 0: combinedDomains.append(min(remainingA, remainingB))
 
   if indexA < polingA.size-1:
     for i in range(indexA, polingA.size):
